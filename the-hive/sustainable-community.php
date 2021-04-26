ff1a558e04cf98458331185a052fd688af78f4d2
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-04-26 09:47:24 +0100

    added honeybee village page

--- the-hive/sustainable-community.php
+++ the-hive/sustainable-community.php
@@ -15,6 +15,28 @@
 <?php BeeArcApi::include("../components/header.php", $headerVars); ?>
 
 <main>
+    <div class="l-block l-block--small">
+        <div class="l-row l-row--centered">
+            <div class="l-flow">
+                <h2 class="c-h2 c-h2--theme">
+                    The BeeArc Sustainable Community
+                </h2>
+                <p>
+                    Developing a model of cooperative Work and Living practice.
+                </p>
+                <ul>
+                    <li>Accommodation for shared living</li>
+                    <li>Community run Village Store</li>
+                    <li>Community Produce Exchange</li>
+                    <li>Community run small scale sustainable workshops</li>
+                    <li>Community run Garden and Physic Garden</li>
+                    <li>Community run Beekeeping</li>
+                    <li>Community run complementary health  clinic</li>
+                    <li>Community run transport</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
 </main>
 
